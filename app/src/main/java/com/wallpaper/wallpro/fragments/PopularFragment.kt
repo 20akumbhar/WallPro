9320043734393a53fc93ffd231b7424ec7b36f37
20akumbhar <20akumbhar@gmail.com>
2021-06-29 21:07:38 +0530

    finished popular page pagination

--- app/src/main/java/com/wallpaper/wallpro/fragments/PopularFragment.kt
+++ app/src/main/java/com/wallpaper/wallpro/fragments/PopularFragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -18,8 +21,10 @@ import com.wallpaper.wallpro.adapters.WallpaperAdapter
 import com.wallpaper.wallpro.models.Wallpaper
 
 class PopularFragment : Fragment() {
-
-
+    private var lastDocument: DocumentSnapshot?=null
+    private lateinit var progressbar: ProgressBar
+    private lateinit var wallpaperList: MutableList<Wallpaper>
+    private lateinit var adapter: WallpaperAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,17 +35,28 @@ class PopularFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val wallpaperList = mutableListOf<Wallpaper>()
-        var isFirst=true
-        val recyclerView: RecyclerView =view.findViewById(R.id.home_recyclerView)
-        val adapter= WallpaperAdapter(requireActivity(), wallpaperList)
-        recyclerView.adapter=adapter
-        recyclerView.layoutManager= GridLayoutManager(view.context,2)
+        wallpaperList = mutableListOf()
+        var isFirst = true
+        val recyclerView: RecyclerView = view.findViewById(R.id.home_recyclerView)
+        progressbar = view.findViewById(R.id.popular_progressbar)
+        adapter = WallpaperAdapter(requireActivity(), wallpaperList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(view.context, 2)
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!recyclerView.canScrollVertically(View.SCROLL_INDICATOR_BOTTOM)) {
+                    loadMoreWallpapers()
+                }
+            }
+        })
+
 
         Firebase.firestore.collection("wallpapers")
-            .orderBy("timestamp",Query.Direction.DESCENDING)
-            .limit(16)
-            .whereEqualTo("isPopular",true)
+            .orderBy("timestamp", Query.Direction.DESCENDING)
+            .limit(12)
+            .whereEqualTo("isPopular", true)
             .addSnapshotListener { snapshots, e ->
                 if (e != null) {
                     Log.w("Firebase  :", "listen:error", e)
@@ -63,17 +79,76 @@ class PopularFragment : Fragment() {
                                 )
                             if (isFirst) {
                                 wallpaperList.add(wallpaper)
-                            }else{
-                                wallpaperList.add(0,wallpaper)
+                            } else {
+                                wallpaperList.add(0, wallpaper)
                             }
+
                             Log.d("Firebase  Added:", "New city: ${dc.document.data}")
                         }
+//                        DocumentChange.Type.REMOVED->{
+//                            val document = dc.document
+//
+//                            val wallpaper =
+//                                Wallpaper(
+//                                    document.id,
+//                                    document.data["image"].toString(),
+//                                    document.data["thumbnail"].toString(),
+//                                    document.data["isPopular"] as Boolean,
+//                                    document.data["source"].toString(),
+//                                    document.data["userId"].toString(),
+//                                    document.data["categoryId"].toString(),
+//                                    document.data["timestamp"] as Timestamp,
+//                                )
+//                            val p=wallpaperList.indexOf(wallpaper)
+//                            adapter.notifyItemRemoved(p)
+//                        }
                         else -> {
                         }
                     }
                 }
+                if (isFirst){
+                    Log.d("assigning ","last doc")
+                    lastDocument = snapshots.documents[snapshots.size()-1]
+                }
                 isFirst = false
                 adapter.notifyDataSetChanged()
             }
+    }
+
+    private fun loadMoreWallpapers() {
+        progressbar.visibility = View.VISIBLE
+        Log.d("random data:", "started")
+        Firebase.firestore.collection("wallpapers")
+            .orderBy(FieldPath.documentId())
+            .whereEqualTo("isPopular", true)
+            .startAfter(lastDocument!!)
+            .limit(10)
+            .get()
+            .addOnSuccessListener { snapshots ->
+                for (dc in snapshots!!) {
+
+                    val wallpaper =
+                        Wallpaper(
+                            dc.id,
+                            dc.data["image"].toString(),
+                            dc.data["thumbnail"].toString(),
+                            dc.data["isPopular"] as Boolean,
+                            dc.data["source"].toString(),
+                            dc.data["userId"].toString(),
+                            dc.data["categoryId"].toString(),
+                            dc.data["timestamp"] as Timestamp,
+                        )
+                    lastDocument = dc
+                    wallpaperList.add(wallpaper)
+                    Log.d("random data:", wallpaper.id)
+
+                }
+                adapter.notifyDataSetChanged()
+                progressbar.visibility = View.GONE
+            }
+            .addOnFailureListener {
+                Log.e("firebase", it.message!!)
+            }
+
     }
 }
